0b5c4d48c8f33596c8373dd2523bd8d55033076b
s18889 <s18889@LAB003-03>
2020-03-09 11:17:58 +0100

    11:17

---
+++ cw2/cw2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace cw2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = "data/dane.csv";
+
+            FileInfo f = new FileInfo(file);
+            StreamReader stream = new StreamReader(f.OpenRead());
+
+            string line = "";
+            while((line = stream.ReadLine())!= null)
+            {
+                Console.WriteLine(line);
+            }
+
+
+
+            stream.Dispose();//... IDisposable
+        }
+    }
+}
